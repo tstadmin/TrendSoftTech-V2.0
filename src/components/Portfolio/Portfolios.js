46534354a7fbf359846ed72b75fb5a613d50e0ed
praveeny1996 <praveen.y@trendsoftteach.co.in>
2022-12-20 17:59:22 +0530

    changes done in the components

--- src/components/Portfolio/Portfolios.js
+++ src/components/Portfolio/Portfolios.js
@@ -9,7 +9,7 @@ const Portfolios = props => {
       </h1>
       {props.portfolio.map((i, idx) => (
         <div className="text-4xl md:text-7xl ml-16 group 540Screen:flex grid justify-start 540Screen:space-x-[50%] space-y-20">
-          <h1 className="myDIV duration-500 hover:cursor-pointer hover:text-white hover:bg-black font-mono mb-6 ">
+          <h1 className="myDIV duration-1000 hover:skew-y-3 hover:cursor-pointer hover:text-white hover:bg-black font-mono mb-6 hover:scale-75 ">
             {i.listItem.title}
           </h1>
           <img
--- src/components/home/Banner.js
+++ src/components/home/Banner.js
@@ -1,10 +1,10 @@
 import React from "react"
 import tw from "twin.macro"
 const Banner = props => {
-  const Images = tw.img`object-cover 2xl:h-[100vh]  w-[100vw] h-[95vh]`
+  const Images = tw.img`object-cover 2xl:h-[100vh]  w-[100vw] h-[95vh] brightness-50`
   const Div = tw.div`lg:flex relative shadow-2xl shadow-black/40`
   const Button = tw.button` text-2xl border  border-blue-500   text-blue-500  font-mono hover:bg-blue-500 hover:text-white font-semibold duration-75  px-14 py-2 mb-1 rounded-full`
-  const InfoDiv = tw.div`space-y-14 bg-black text-white py-24 2xl:w-[60vw]  2xl:h-[100vh] lg:w-[60vw]  lg:h-[95vh] md:h-[80vh] h-[80vh] sm:pl-20 lg:pl-48 pl-8 lg:pt-48`
+  const InfoDiv = tw.div`space-y-14 text-white py-24 2xl:w-[60vw]  2xl:h-[100vh] lg:w-[60vw]  lg:h-[95vh] md:h-[80vh] h-[80vh] sm:pl-20 lg:pl-48 pl-8 lg:pt-48`
   const H1 = tw.h1` 2xl:text-8xl xl:text-7xl md:text-5xl sm:text-6xl text-4xl  font-bold font-mono`
   const H2 = tw.h1` 2xl:text-3xl md:text-2xl text-xl font-normal font-mono`
   const ImgeDiv = tw.div`absolute -z-20 hidden  lg:block`
@@ -59,7 +59,7 @@ const Banner = props => {
       {/* absolute -z-20 340Screen:hidden lg:block */}
       <ImgeDiv>
         <Images
-          src="https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80"
+          src="https://static.vecteezy.com/system/resources/previews/008/484/147/original/business-people-team-work-laptop-computer-office-workplace-free-vector.jpg"
           alt="bannerImage"
         />
       </ImgeDiv>
