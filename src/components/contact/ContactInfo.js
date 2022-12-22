a102bb6ef56565ef0772cb04bcb23bc9afdb3e67
praveeny1996 <praveen.y@trendsoftteach.co.in>
2022-12-22 10:23:59 +0530

    addess component

--- src/components/contact/ContactInfo.js
+++ src/components/contact/ContactInfo.js
@@ -2,35 +2,36 @@ import React from "react"
 
 import { MdOutlineEmail } from "react-icons/md"
 
+import { IoLocationSharp } from "react-icons/io5"
+
 const ContactInfo = () => {
   return (
-    <div>
-      <h1 className="text-center mt-14 text-6xl">Contact</h1>
-      <div className="flex">
+    <div className="space-y-6">
+      <div className="flex space-x-4 ">
         <MdOutlineEmail />
-        <div>
+        <div className="space-y-5">
           <>
-            <h1>Email: </h1>
+            <h1 className="font-semibold">Email: </h1>
             <h1>info@trendsofttech.com</h1>
           </>
           <>
-            <h1>For Business Inquiries</h1>
+            <h1 className="font-semibold">For Business Inquiries: </h1>
             <h1>service@trendsofttech.com</h1>
           </>
         </div>
       </div>
-      <div className="flex">
-        <MdOutlineEmail />
-        <div>
+      <div className="flex space-x-4">
+        <IoLocationSharp />
+        <div className="space-y-5">
           <>
-            <h1>Address: </h1>
+            <h1 className="font-semibold">Address: </h1>
             <h1>
               Flat No.301 3rd Floor MIG-37 Vasavi Srinivasam Apartment KPHB
               Colony Road No.1 Kukatpally Hyderabad-500072
             </h1>
           </>
           <>
-            <h1>Address 2:</h1>
+            <h1 className="font-semibold">Address 2:</h1>
             <h1>
               Unit No: 301-302 NRT Tech Park Mangalagiri Bypass Amaravati-522503
             </h1>
