f52d00896cb27b5a929da591fc245e1d8be4e7f4
Ganesh18547 <115688249+Ganesh18547@users.noreply.github.com>
2022-12-30 18:23:48 +0530

    change done in 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,12 +6,12 @@ import { Link } from "gatsby"
 const NotFoundPage = () => (
   <div className="relative">
     <img
-      className="h-[100vh] w-full object-cover "
+      className="h-[100vh] w-full object-cover"
       src={errorImage}
       alt="404 page image"
     />
     <Link to="/">
-      <h1 className="absolute left-[45%] top-[79%] text-white hover:cursor-pointer ring-4 p-4 ring-white rounded-lg hover:ring-blue-400 hover:text-blue-400 ">
+      <h1 className="absolute sm:left-[45%] left-[30%] top-[79%] text-white hover:cursor-pointer ring-4 p-4 ring-white rounded-lg hover:ring-blue-400 hover:text-blue-400 ">
         Go To Home Page
       </h1>
     </Link>
