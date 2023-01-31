6997ff8748df578cbc3ad6b5e9765602413d0748
praveeny1996 <praveen.y@trendsoftteach.co.in>
2023-01-31 17:07:53 +0530

    chnages done in navbar header and client images

--- src/components/home/Banner.js
+++ src/components/home/Banner.js
@@ -9,9 +9,9 @@ const Banner = props => {
         style={{
           backgroundImage: `url(${background})`,
         }}
-        className="font-Poppins xl:h-[calc(100vh-137px)] md:h-[calc(60vh-137px)] sm:h-[calc(60vh-137px)] 540Screen:h-[calc(60vh-137px)] 440Screen:h-[calc(60vh-137px)] 340Screen:h-[calc(60vh-137px)]  h-[calc(120vh-137px)] bg-no-repeat bg-cover bg-center  "
+        className=" font-Poppins xl:h-[calc(100vh-137px)] md:h-[calc(60vh-137px)] sm:h-[calc(60vh-137px)] 540Screen:h-[calc(60vh-137px)] 440Screen:h-[calc(60vh-137px)] 340Screen:h-[calc(60vh-137px)]  h-[calc(120vh-137px)] bg-no-repeat bg-cover bg-center  "
       >
-        <center className="  xl:pt-[15%] 540Screen:pt-[10%] 440Screen:pt-[18%] 340Screen:pt-[25%] pt-[4%]  540Screen:space-y-16 space-y-6  text-white  ">
+        <center className="  xl:pt-[15%] 540Screen:pt-[10%] 440Screen:pt-[18%] 340Screen:pt-[25%] pt-[4%]  540Screen:space-y-16 space-y-6  text-white ">
           <div
             className="animate__animated animate__fadeInLeft  1920Screen:w-[30vw] xl:w-[40vw] lg:w-[40vw] md:w-[50vw] sm:w-[60vw] 540Screen:w-[80vw] 440Screen:w-[60vw]  324Screen:w-[250px] 240Screen:w-[200px]  "
             dangerouslySetInnerHTML={{ __html: props.banner.headers }}
