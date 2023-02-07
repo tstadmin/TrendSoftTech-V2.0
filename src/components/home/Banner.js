4311b22033be8e11e6a736325c254b4e40a41c9f
praveeny1996 <praveen.y@trendsoftteach.co.in>
2023-02-07 18:49:21 +0530

    changes done and added carsouel

--- src/components/home/Banner.js
+++ src/components/home/Banner.js
@@ -1,28 +1,24 @@
-import React from "react"
-import background from "../../../static/img/HomeImages/banner.svg"
-import "animate.css"
-const Banner = props => {
-  return (
-    <div id="header" role="banner">
-      <div
-        style={{
-          backgroundImage: `url(${background})`,
-        }}
-        className=" font-Poppins 2048Screen:h-[calc(100vh-137px)] 1920Screen:h-[calc(100vh-137px)] xl:h-[calc(100vh-137px)] md:h-[calc(60vh-60px)] sm:h-[calc(60vh-40px)] 540Screen:h-[calc(60vh-137px)] 440Screen:h-[calc(60vh-137px)] 340Screen:h-[calc(60vh-137px)]  h-[calc(120vh-137px)] bg-no-repeat bg-cover bg-center  "
-      >
-        <center className="  xl:pt-[15%] 540Screen:pt-[10%] 440Screen:pt-[18%] 340Screen:pt-[25%] pt-[4%]  540Screen:space-y-16 space-y-6  text-white ">
-          <div
-            className="animate__animated animate__fadeInLeft  1920Screen:w-[30vw] xl:w-[40vw] lg:w-[40vw] md:w-[50vw] sm:w-[60vw] 540Screen:w-[80vw] 440Screen:w-[60vw]  324Screen:w-[250px] 240Screen:w-[200px]  "
-            dangerouslySetInnerHTML={{ __html: props.banner.headers }}
-          />
+// import React from "react"
+// import "animate.css"
+// const Banner = props => {
+//   return (
+//     <div id="header" role="banner">
+//       <div
+//         style={{
+//           backgroundImage: `url(${background})`,
+//         }}
+//         className=" font-Poppins 2048Screen:h-[calc(100vh-137px)] 1920Screen:h-[calc(100vh-137px)] xl:h-[calc(100vh-137px)] md:h-[calc(60vh-60px)] sm:h-[calc(60vh-40px)] 540Screen:h-[calc(60vh-137px)] 440Screen:h-[calc(60vh-137px)] 340Screen:h-[calc(60vh-137px)]  h-[calc(120vh-137px)] bg-no-repeat bg-cover bg-center  "
+//       >
+//         <center className="  xl:pt-[15%] 540Screen:pt-[10%] 440Screen:pt-[18%] 340Screen:pt-[25%] pt-[4%]  540Screen:space-y-16 space-y-6  text-white ">
+//           <div
+//             className="animate__animated animate__fadeInLeft  1920Screen:w-[30vw] xl:w-[40vw] lg:w-[40vw] md:w-[50vw] sm:w-[60vw] 540Screen:w-[80vw] 440Screen:w-[60vw]  324Screen:w-[250px] 240Screen:w-[200px]  "
+//             dangerouslySetInnerHTML={{ __html: props.banner.headers }}
+//           />
 
-          {/* <div className="animate__animated animate__fadeInLeft lg:text-2xl md:text-lg 340Screen:text-sm text-xs 2xl:w-40vw 540Screen:w-[40vw] 440Screen:w-[60vw] md:w-[35vw] w-[50vw] lg:w-[35vw] 340Screen:font-medium font-normal  ">
-            {props.banner.title}
-          </div> */}
-        </center>
-      </div>
-    </div>
-  )
-}
+//         </center>
+//       </div>
+//     </div>
+//   )
+// }
 
-export default Banner
+// export default Banner
--- src/components/home/Carousel.js
+++ src/components/home/Carousel.js
@@ -1,50 +1,56 @@
-import React, { Component } from 'react';
+import React, { useRef } from "react"
 
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
-import { imagess} from "../../../static/img/HomeImages/banner.svg"
-import { Button } from '@mantine/core';
-import img1 from "../../../static/img/HomeImages/banner.svg"
+import { Carousel } from "@mantine/carousel"
+import Autoplay from "embla-carousel-autoplay"
+
+import { Button } from "@mantine/core"
 import "./HomeStyle.css"
-import { Link } from 'gatsby';
+import { Link } from "gatsby"
 const CarouselItem = () => {
-    const data=[
-        {
-            id:1,
-            img:"/img/HomeImages/banner.svg",
-            description: "We Increase your Business Success"
-        },
-        {
-            id:2,
-            img:"http://qa.trendsofttech.co.in/img/HomeImages/aboutUS.svg",
-            description: "Web Development"
-        },
-        {
-            id:3,
-            img:"http://qa.trendsofttech.co.in/img/HomeImages/aboutUS.svg",
-            description: "Ecommerce Development"
-        }
-    ]
+  const data = [
+    {
+      id: 1,
+      img: "/img/HomeImages/home_banner_1.jpg",
+      description: "We Increase your Business Success",
+    },
+    {
+      id: 2,
+      img: "/img/HomeImages/home_banner_2.jpg",
+      description: "Web Development",
+    },
+    {
+      id: 3,
+      img: "/img/HomeImages/home_banner_3.jpg",
+      description: "Ecommerce Development",
+    },
+  ]
+  const autoplay = useRef(Autoplay({ delay: 6000 }))
+
   return (
     <div>
-       <Carousel autoPlay={true} showArrows={true} showThumbs={false} infiniteLoop={true} transitionTime={900} showIndicators={false} >
-        {
-            data.map((item)=>(
-                <div className='w-[100vw] 2048Screen:h-[calc(70vh-137px)] 1920Screen:h-[calc(70vh-117px)] xl:h-[calc(70vh-137px)] md:h-[calc(60vh-60px)] sm:h-[calc(60vh-40px)] 540Screen:h-[calc(60vh-137px)] 440Screen:h-[calc(60vh-137px)] 340Screen:h-[calc(60vh-137px)] 320Screen:h-[calc(100vh-137px)] h-[calc(40vh-137px)] relative '  >
-                    
-                <img src={item.img} className=""  />
-          
-                    <div className='absolute  sm:top-[210px] top-[25px] 340Screen:top-[150px]   w-[100%] sm:space-y-8 space-y-4'>
-                <p className='text-center text-white  lg:text-3xl md:text-lg sm:text-sm text-xs font-semibold'>{item.description}</p>
-                <div>
-                <Link  to="/contact/" className='bg-blue-700 sm:p-2 hover:cursor-pointer   p-1 rounded-lg  lg:text-xl md:text-lg sm:text-sm text-xs sm:w-[130px] w-[100px] justify-center text-white'>connect</Link>
-                </div>
-                </div>
-                </div>
-            ))
-        }
+      <Carousel
+        withIndicators
+        dragFree
+        loop
+        align="start"
+        transitionDuration="1500"
+        plugins={[autoplay.current]}
+      >
+        {data.map(item => (
+          <Carousel.Slide className="w-[100vw] 2048Screen:h-[calc(70vh-137px)] 1920Screen:h-[calc(70vh-117px)] xl:h-[calc(70vh-137px)] md:h-[calc(60vh-60px)] sm:h-[calc(60vh-40px)] 540Screen:h-[calc(60vh-137px)] 440Screen:h-[calc(60vh-137px)] 340Screen:h-[calc(60vh-137px)] 320Screen:h-[calc(100vh-137px)] h-[calc(40vh-137px)] relative ">
+            <img src={item.img} />
 
-            </Carousel>
+            <div className="absolute sm:top-[210px] top-[25px] 340Screen:top-[150px]   w-[100%] sm:space-y-8 space-y-4">
+              <p className="text-center text-white  lg:text-3xl md:text-lg sm:text-sm text-xs font-semibold">
+                {item.description}
+              </p>
+              {/* <p className="text-center bg-blue-700 sm:p-2 hover:cursor-pointer   p-1 rounded-lg  lg:text-xl md:text-lg sm:text-sm text-xs sm:w-[130px] w-[100px]  text-white">
+                Learn More
+              </p> */}
+            </div>
+          </Carousel.Slide>
+        ))}
+      </Carousel>
     </div>
   )
 }
